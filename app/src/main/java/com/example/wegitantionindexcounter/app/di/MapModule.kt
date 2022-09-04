14b7adf3f12b93ee6bc2d0410e90cdbd74b1d9ba
Panicia <kkk2224@yandex.ru>
2022-09-04 14:07:33 +0300

    module changes

--- app/src/main/java/com/example/wegitantionindexcounter/app/di/MapModule.kt
+++ app/src/main/java/com/example/wegitantionindexcounter/app/di/MapModule.kt
@@ -1,17 +1,18 @@
 package com.example.wegitantionindexcounter.app.di
 
-import android.view.LayoutInflater
-import com.example.wegitantionindexcounter.MainActivity
-import com.example.wegitantionindexcounter.databinding.ActivityMainBinding
+import models.MapRepository
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import org.osmdroid.views.MapView
 import viewModels.MapViewModel
 
 val viewModelModule = module {
-    viewModel { MapViewModel() }
+    viewModel { MapViewModel(get()) }
 }
 
-val mapModule = module {
+val repositoryModule = module {
+    fun provideRepository(): MapRepository {
+        return MapRepository()
+    }
 
+    single { provideRepository() }
 }
